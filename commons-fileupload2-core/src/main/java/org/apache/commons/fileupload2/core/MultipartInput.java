7c76d97090fa558522e2cc4f3773d0fdc3484448
Saliha192 <s.awan@studenti.unisa.it>
2025-07-07 01:34:26 +0200

    Removed the declaration of thrown exception line 843 in MultipartInput.java

--- commons-fileupload2-core/src/main/java/org/apache/commons/fileupload2/core/MultipartInput.java
+++ commons-fileupload2-core/src/main/java/org/apache/commons/fileupload2/core/MultipartInput.java
@@ -840,7 +840,7 @@ public final class MultipartInput {
      * @throws MalformedStreamException if the stream ends unexpectedly.
      * @throws IOException              if an i/o error occurs.
      */
-    public long readBodyData(final OutputStream output) throws MalformedStreamException, IOException {
+    public long readBodyData(final OutputStream output) throws IOException {
         try (var inputStream = newInputStream()) {
             return IOUtils.copyLarge(inputStream, output);
         }
